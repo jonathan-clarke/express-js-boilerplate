2e2d22ffee52097ff4460304fef700ded5072938
jonathan-clarke <jon.daniel.clarke@gmail.com>
2025-07-17 22:00:11 -0400

    fix(script): fixed the debugging config for running script.ts

--- src/script.ts
+++ src/script.ts
@@ -1,18 +1,27 @@
-import { setTimeout } from 'node:timers/promises';
-import axios from 'axios';
-
-let breakScheduler: boolean = false;
-let delay: number = 1000;
-let counter: number = 0;
-
-(async () => {
-  while (!breakScheduler) {
-    await setTimeout(delay);
-    console.log('delay');
-    console.log('counter', counter);
-    counter = counter + 1;
-    if (counter === 10) {
-      breakScheduler = true;
+const reverseString = (stringToReverse: string) => {
+  let leftPointer = 0;
+  let rightPointer = stringToReverse.length - 1;
+  const newStringArray = [];
+  while (leftPointer <= rightPointer) {
+    if (leftPointer < rightPointer) {
+      newStringArray[rightPointer] = stringToReverse[leftPointer];
     }
+    newStringArray[leftPointer] = stringToReverse[rightPointer];
+    leftPointer++;
+    rightPointer--;
   }
-})();
+
+  return newStringArray.join('');
+};
+
+const string1 = 'cata';
+
+const reversedString1 = reverseString(string1);
+
+console.log(reversedString1);
+
+const string2 = 'cat';
+
+const reversedString2 = reverseString(string2);
+
+console.log(reversedString2);
